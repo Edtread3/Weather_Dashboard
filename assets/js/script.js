e271893a094928dc3802b8e266df5225a347766f
unknown <edtread3@gmail.com>
2023-06-30 17:20:13 -0400

    added 5-day-forecast to weather dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,65 @@
 const apiKey = "5ccf588deb3a55fdba751a7c9991a3a7";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=new york";
+
+function celsiusToFahrenheit(celsius) {
+    var fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit.toFixed(0);
+  }
+
+async function fiveDayForecast(){
+    var searchCity = document.getElementById("text-city").value.trim();
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${searchCity}&appid=${apiKey}`;
+
+    const response = await fetch(forecastUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+
+    const cardTwo = document.getElementById("weather-two");
+    var fahrenheit = celsiusToFahrenheit(data.list[1].main.temp);
+        document.querySelector(".temperature-2").innerHTML = fahrenheit + "°F";
+        document.querySelector(".description-2").innerHTML = data.list[1].weather[0].main;
+
+    const cardThree = document.getElementById("weather-three");
+    var fahrenheit = celsiusToFahrenheit(data.list[2].main.temp);
+    document.querySelector(".temperature-3").innerHTML = fahrenheit + "°F";
+    document.querySelector(".description-3").innerHTML = data.list[2].weather[0].main;
+   
+    const cardFour = document.getElementById("weather-four");
+    var fahrenheit = celsiusToFahrenheit(data.list[3].main.temp);
+    document.querySelector(".temperature-4").innerHTML = fahrenheit + "°F";
+    document.querySelector(".description-4").innerHTML = data.list[3].weather[0].main;
+
+    const cardFive = document.getElementById("weather-five");
+    var fahrenheit = celsiusToFahrenheit(data.list[4].main.temp);
+    document.querySelector(".temperature-5").innerHTML = fahrenheit + "°F";
+    document.querySelector(".description-5").innerHTML = data.list[4].weather[0].main;
+    
+}  
 
 async function checkWeather(){
+    var searchCity = document.getElementById("text-city").value.trim();
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${searchCity}`;
+
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
+
     console.log(data)
-
+    var fahrenheit = celsiusToFahrenheit(data.main.temp);
+    
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".temp").innerHTML = fahrenheit + "°F";
     document.querySelector(".description").innerHTML = data.weather[0].main;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".humidity").innerHTML = "humidity=" + data.main.humidity;
+  
+  
 
+  
 
 
 }
+fiveDayForecast();
 checkWeather();
+
+
+var citySearchBtn = document.getElementById("search-city");
+citySearchBtn.addEventListener("click", checkWeather);
+citySearchBtn.addEventListener("click", fiveDayForecast);
